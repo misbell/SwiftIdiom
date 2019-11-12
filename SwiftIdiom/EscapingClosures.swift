51de770412a1d9e781ff5b0b13f0273177c2f942
michael isbell <prenezisbell@gmail.com>
2019-11-12 17:30:59 -0500

    first commit

---
+++ SwiftIdiom/EscapingClosures.swift
@@ -0,0 +1,9 @@
+//
+//  EscapingClosures.swift
+//  SwiftIdiom
+//
+//  Created by michael isbell on 11/12/19.
+//  Copyright © 2019 Advanced Mobile Development. All rights reserved.
+//
+
+import Foundation
